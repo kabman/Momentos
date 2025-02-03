3c3bbb8f9ba282e0dee7cfe362b8c30f12fd1861
kabir lamin <kabirlamin1997@gmail.com>
2025-02-03 13:31:08 +0100

    a

--- frontend/app/createaccount/page.jsx
+++ frontend/app/createaccount/page.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import CreateAccount from './createaccount';
+import CreateAccount from './createaccount.jsx';
 
 export default function CreateAccountPage() {
     const router = useRouter();
